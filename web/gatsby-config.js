086800fcf70171b6e39646a51c2a0785c8854863
Saleh meraj <saleh@Salehs-MacBook-Air.local>
2021-08-16 17:53:46 +0300

    fix: run analyser only with local build

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -7,12 +7,14 @@ const clientConfig = require("./client-config");
 
 const isProd = process.env.NODE_ENV === "production";
 
+let conditionalPlugin = []
+if (process.env.RUN_ANALYSER)
+  conditionalPlugin.push("gatsby-plugin-webpack-bundle-analyser-v2")
 module.exports = {
   plugins: [
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-  "gatsby-plugin-webpack-bundle-analyser-v2" ,
     {
       resolve: "gatsby-source-sanity",
       options: {
@@ -22,5 +24,6 @@ module.exports = {
         overlayDrafts: !isProd,
       },
     },
+    ...conditionalPlugin
   ],
 }
