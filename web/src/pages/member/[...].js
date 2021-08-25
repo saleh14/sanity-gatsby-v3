9090ede9da06811632ff0335d3add42b0e70813b
Saleh meraj <saleh@Salehs-MacBook-Air.local>
2021-08-26 02:12:35 +0300

    avoid conflect for netlify build

--- web/src/pages/member/[...].js
+++ web/src/pages/member/[...].js
@@ -1,33 +1,35 @@
 import React, { useContext, useEffect } from 'react'
 import { Router } from '@reach/router'
 import { NhostAuthProvider } from '@nhost/react-auth'
-import { auth } from '../../lib/nhost'
-import Login from '../../components/Login'
-import Container from '../../components/container'
-import GraphQLErrorList from '../../components/graphql-error-list'
-import Seo from '../../components/seo'
-import Layout from '../../containers/layout'
-import MemberDefault from '../../components/memberDefault'
-import { MemberContext } from '../../lib/MemberContext'
-import { NhostUrqlProvider } from '../../lib/NhostUrqlProvider'
+// import { auth } from '../../lib/nhost'
+// import Login from '../../components/Login'
+// import Container from '../../components/container'
+// import GraphQLErrorList from '../../components/graphql-error-list'
+// import Seo from '../../components/seo'
+// import Layout from '../../containers/layout'
+// import MemberDefault from '../../components/memberDefault'
+// import { MemberContext } from '../../lib/MemberContext'
+// import { NhostUrqlProvider } from '../../lib/NhostUrqlProvider'
 
 const App = () => {
   return (
-    <Layout>
-      <Container>
-        <NhostAuthProvider auth={auth}>
-          <NhostUrqlProvider
-            auth={auth}
-            gqlEndpoint="https://hasura-e7c479eb.nhost.app/v1/graphql"
-          >
-            <Router basepath="/member">
-              <Login path="/login" />
-              <MemberDefault path="/" />
-            </Router>
-          </NhostUrqlProvider>
-        </NhostAuthProvider>
-      </Container>
-    </Layout>
+    <div>hello</div>
+
+    // <Layout>
+    //   <Container>
+    //     <NhostAuthProvider auth={auth}>
+    //       <NhostUrqlProvider
+    //         auth={auth}
+    //         gqlEndpoint="https://hasura-e7c479eb.nhost.app/v1/graphql"
+    //       >
+    //         <Router basepath="/member">
+    //           <Login path="/login" />
+    //           <MemberDefault path="/" />
+    //         </Router>
+    //       </NhostUrqlProvider>
+    //     </NhostAuthProvider>
+    //   </Container>
+    // </Layout>
   )
 }
 
