989178bf15e66653bd2dac58d26d3c77c5fedaf2
Saleh meraj <saleh@Salehs-MacBook-Air.local>
2021-08-27 05:06:25 +0300

    config gatsby cloud plugin

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,29 +1,30 @@
 // Load variables from `.env` as soon as possible
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV || "development"}`,
-});
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV || 'development'}`,
+})
 
-const clientConfig = require("./client-config");
+const clientConfig = require('./client-config')
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production'
 
 let conditionalPlugin = []
 if (process.env.RUN_ANALYSER)
-  conditionalPlugin.push("gatsby-plugin-webpack-bundle-analyser-v2")
+  conditionalPlugin.push('gatsby-plugin-webpack-bundle-analyser-v2')
 module.exports = {
   plugins: [
-    `gatsby-plugin-netlify`,
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-preact',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-brotli',
       options: {
-        extensions: ['css', 'html', 'js', 'svg']
-      }
+        extensions: ['css', 'html', 'js', 'svg'],
+      },
     },
     {
-      resolve: "gatsby-source-sanity",
+      resolve: 'gatsby-source-sanity',
       options: {
         ...clientConfig.sanity,
         token: process.env.SANITY_READ_TOKEN,
@@ -31,6 +32,6 @@ module.exports = {
         overlayDrafts: !isProd,
       },
     },
-    ...conditionalPlugin
+    ...conditionalPlugin,
   ],
 }
